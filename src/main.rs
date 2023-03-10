8a1a242373b020cbe211996811ac9048ab0191af
denimcouch <alexmata92@ymail.com>
2023-03-10 13:37:31 -0600

    ask for user input and store that value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Guessing Game
+    // Step 1: Ask user for input
+    println!("Guess the number!");
+    
+    println!("Please input your guess: ");
+    
+    let mut guess = String::new();
+    
+    // Step 2: Process the input and check that the input is in the expected form
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guess: {guess}");
 }
