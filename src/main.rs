c2ea956d1067d0843f30d0f4d7f65a0a9cb747a4
denimcouch <alexmata92@ymail.com>
2023-03-10 14:12:23 -0600

    error handling makes everything nice

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ fn main() {
     // Step 1: Ask user for input
     println!("Guess the number!");
 
+    // Generate a random number between 1 and 100
     let secret_number = rand::thread_rng()
-                                  .gen_range(1..=100); // Generate a random number between 1 and 100
+                                  .gen_range(1..=100); 
     
     loop {
         println!("Please input your guess: ");
@@ -20,7 +21,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a valid number.");
+                println!("\n");
+                continue
+            },
+        };
     
         println!("You guessed: {guess}");
     
