aa1ea714de536ed1e86ff703cceaab599e476af3
denimcouch <alexmata92@ymail.com>
2023-03-10 14:00:55 -0600

    build core game loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -9,17 +10,29 @@ fn main() {
     let secret_number = rand::thread_rng()
                                   .gen_range(1..=100); // Generate a random number between 1 and 100
     
-    // println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess: ");
+    loop {
+        println!("Please input your guess: ");
+        
+        let mut guess = String::new();
+        
+        // Step 2: Process the input and check that the input is in the expected form
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
     
-    let mut guess = String::new();
+        let guess: u32 = guess.trim().parse().expect("Please type a number.");
     
-    // Step 2: Process the input and check that the input is in the expected form
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guess: {guess}");
+        println!("You guessed: {guess}");
+    
+        // Step 3: Compare the secret number and user input values
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 
 }
